27294080932df662f88dc7ef7a72c34545857649
TieuCotDau <36128448+HaVu279@users.noreply.github.com>
2019-11-24 22:22:17 +0700

    Add files via upload

---
+++ B3/merge.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from os import path
+
+red = Image.open('red_image.png')
+green = Image.open('green_image.png')
+blue = Image.open('blue_image.png')
+
+if(path.exists('anpha_image.png')):
+    anpha = Image.open('anpha_image.png')
+    img = Image.merge("RGBA", (red, green, blue, anpha))
+else:
+    img = Image.merge("RGB", (red, green, blue))
+
+img.save('image.png')
